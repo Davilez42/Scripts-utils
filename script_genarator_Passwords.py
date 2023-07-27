da34b5e3e81627991d7a528eb2b3e1dcdbf587fa
Jose David Suarez Cardona <josedavid4227@gmail.com>
2023-07-26 19:38:17 -0500

    refactor:

--- script_genarator_Passwords.py
+++ script_genarator_Passwords.py
@@ -1,15 +1,6 @@
 from random import randint
 from string import ascii_lowercase
 """
- * Escribe un programa que sea capaz de generar contraseñas de forma aleatoria.
- * Podrás configurar generar contraseñas con los siguientes parámetros:
- * - Longitud: Entre 8 y 16.
- * - Con o sin letras mayúsculas.
- * - Con o sin números.
- * - Con o sin símbolos.
- * (Pudiendo combinar todos estos parámetros entre ellos)
- """
-"""
 * Funcionamiento
 * Comandos
 * -l : genera contraseña con longitud entre 8 a 16 caracteres
