ef60bee64d6c19b5a39f7a8325ae0ca042f2cf59
Davilez42 <josedavid4227@gmail.com>
2024-09-08 11:47:44 -0500

    refactor:

--- script_delete_folders_empty.py
+++ script_delete_folders_empty.py
@@ -1,41 +1,40 @@
-from os import walk,remove,removedirs,system
+from os import walk, remove, removedirs, system
 import datetime
-import time 
+import time
+
 
 def generarInforme(texto):
-    print('[+] Terminado. Deseas guardar un informe ? (s/n)::',end=" ")
+    print('[+] Terminado. Deseas guardar un informe ? (s/n)::', end=" ")
     while True:
-            dec = str(input(''))
-            if dec == 's' or dec =='S':
-                fecha = str(datetime.datetime.now()).split()
-                hora = fecha[1].split('.')[0].replace(':','_')              
-                with open(f'informe_{fecha[0]}_{hora}.txt','w') as informe:
-                    informe.write(texto)
-                    informe.close()
-                
-                print('Informe Guardado')
-                system('pause')
-                break
-            elif dec == 'n' or dec == 'N':
-                break     
-            print('Porfavor ingrese una opcion correcta.. ')    
-   
-            
+        dec = str(input(''))
+        if dec == 's' or dec == 'S':
+            fecha = str(datetime.datetime.now()).split()
+            hora = fecha[1].split('.')[0].replace(':', '_')
+            with open(f'informe_{fecha[0]}_{hora}.txt', 'w') as informe:
+                informe.write(texto)
+                informe.close()
+
+            print('Informe Guardado')
+            system('pause')
+            break
+        elif dec == 'n' or dec == 'N':
+            break
+        print('Porfavor ingrese una opcion correcta.. ')
+
+
 def eliminarCarpetas():
     cnt_vacias = 0
-    text = '---------------CARPETAAS--ELIMINADAS-------------------' +'\n'  
+    text = '---------------CARPETAAS--ELIMINADAS-------------------' + '\n'
     print(f'[-] Eliminando....')
-    for dirpath,dirname,file in walk('./'):      
-        if len(file) ==0 and len(dirname)==0:   
-            text += dirpath + '\n'  
-            removedirs(dirpath)          
-            cnt_vacias+=1
+    for dirpath, dirname, file in walk('./'):
+        if len(file) == 0 and len(dirname) == 0:
+            text += dirpath + '\n'
+            removedirs(dirpath)
+            cnt_vacias += 1
     system('cls')
-    print(f'Total Carpetas eliminadas:{cnt_vacias}')       
-    generarInforme(text)           
-  
-  
-                             
+    print(f'Total Carpetas eliminadas:{cnt_vacias}')
+    generarInforme(text)
+
+
 if __name__ == '__main__':
     eliminarCarpetas()
-        
